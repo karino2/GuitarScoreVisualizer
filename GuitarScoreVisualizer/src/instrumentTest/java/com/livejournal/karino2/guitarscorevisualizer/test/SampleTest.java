20ed9039eca792a1ee2cc752276c08d32da5cc4c
karino <hogeika2@gmail.com>
2013-07-26 13:12:27 +0900

    Initial import.

---
+++ GuitarScoreVisualizer/src/instrumentTest/java/com/livejournal/karino2/guitarscorevisualizer/test/SampleTest.java
@@ -0,0 +1,12 @@
+package com.livejournal.karino2.guitarscorevisualizer.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by karino on 7/26/13.
+ */
+public class SampleTest extends TestCase {
+    public void testHello() {
+        assertEquals(true, true);
+    }
+}
