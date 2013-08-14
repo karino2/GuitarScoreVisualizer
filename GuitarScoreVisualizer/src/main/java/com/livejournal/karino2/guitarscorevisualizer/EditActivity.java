012b900e04c96d65cbed09231f69c10de8dd9a39
karino <hogeika2@gmail.com>
2013-08-14 12:24:24 +0900

    Change save button to ActionBar.

--- GuitarScoreVisualizer/src/main/java/com/livejournal/karino2/guitarscorevisualizer/EditActivity.java
+++ GuitarScoreVisualizer/src/main/java/com/livejournal/karino2/guitarscorevisualizer/EditActivity.java
@@ -1,7 +1,11 @@
 package com.livejournal.karino2.guitarscorevisualizer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,20 +20,8 @@ public class EditActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        ((Button)findViewById(R.id.buttonSave)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveScoreIfNecessary();
-                setResult(Activity.RESULT_OK);
-                finish();
-            }
-        });
-        ((Button)findViewById(R.id.buttonCancel)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         long id = getIntent().getLongExtra(ARG_ITEM_ID, -1);
         if(id == -1) {
@@ -44,8 +36,35 @@ public class EditActivity extends Activity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     Score score;
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_edit:
+                saveScoreIfNecessary();
+                setResult(Activity.RESULT_OK);
+                finish();
+                return true;
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpTo(this, new Intent(this, ScoreListActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private void saveScoreIfNecessary() {
         EditText etTitle = (EditText)findViewById(R.id.editTextTitle);
